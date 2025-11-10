d81b0e1ba1ea08d245f13f8a31fe16a9143d13c2
antvu <antvu@skbkontur.ru>
2025-11-10 17:37:41 +0500

    Добавил занятие по БД

---
+++ L03-db-via-ado/Consts.cs
@@ -0,0 +1,13 @@
+﻿namespace L03_db_via_ado;
+
+public static class Consts
+{
+    public const string DefaultServer = @"(LocalDB)\MSSQLLocalDB";
+    public const string DefaultDatabase = "master";
+    public const string DefaultAuthSchema = "Trusted_Connection=True";
+
+    public static string GetDbConnectionString() =>
+        $"Server={DefaultServer};Database={DefaultDatabase};{DefaultAuthSchema};";
+
+    public const string PersonsTable = "Persons";
+}
---
+++ L03-db-via-ado/StorageTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace L03_db_via_ado;
+
+public class StorageTests
+{
+    private readonly Storage storage = new(Consts.GetDbConnectionString());
+    
+    [Test]
+    public async Task OpenConnection_Master_ShouldNotThrow()
+    {
+        var checkConnection = async () => await storage.EnsureConnectionSuccess();
+
+        await checkConnection.Should().NotThrowAsync();
+    }
+    
+    [Test]
+    public async Task CreateTable_Persons_ShouldNotThrow()
+    {
+        var createTable = async () => await storage.CreateTable(Consts.PersonsTable);
+
+        await createTable.Should().NotThrowAsync();
+    }
+}
