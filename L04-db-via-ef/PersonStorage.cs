5126dc194af4a790f40097a66b47d38a536231d4
antvu <antvu@skbkontur.ru>
2025-11-18 11:39:58 +0500

    Добавил занятие по БД EF

---
+++ L04-db-via-ef/PersonStorage.cs
@@ -0,0 +1,39 @@
+﻿using L04_db_via_ef.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L04_db_via_ef;
+
+public class PersonStorage
+{
+    private readonly Db storage;
+
+    public PersonStorage(Db storage)
+    {
+        this.storage = storage;
+    }
+
+    public async Task CreatePerson(Person person)
+    {
+        //AddAsync()
+        //SaveChanges()
+        throw new NotImplementedException();
+    }
+
+    public async Task<Person?> GetPerson(Guid id)
+    {
+        //FirstOrDefaultAsync()
+        throw new NotImplementedException();
+    }
+    
+    public async Task<IList<Person>> SearchPersonsByName(string name)
+    {
+        //ToListAsync()
+        throw new NotImplementedException();
+    }
+
+    public async Task Delete(Guid id)
+    {
+        //ExecuteDeleteAsync()
+        throw new NotImplementedException();
+    }
+}
