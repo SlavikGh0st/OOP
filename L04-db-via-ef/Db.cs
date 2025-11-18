b7c33899bae75aa24a4673bcbf1501dbc1c94ad2
antvu <antvu@skbkontur.ru>
2025-11-18 11:39:58 +0500

    Добавил занятие по БД EF

---
+++ L04-db-via-ef/Db.cs
@@ -0,0 +1,15 @@
+﻿using L03_db_via_ado;
+using L04_db_via_ef.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L04_db_via_ef;
+
+public class Db : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(Consts.DefaultConnection);
+    }
+}
---
+++ L04-db-via-ef/PersonStorage.cs
@@ -0,0 +1,38 @@
+﻿using L04_db_via_ef.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L04_db_via_ef;
+
+public class PersonStorage
+{
+    private readonly Db storage;
+
+    public PersonStorage(Db storage)
+    {
+        this.storage = storage;
+    }
+
+    public async Task CreatePerson(Person person)
+    {
+        await storage.Persons.AddAsync(person);
+        await storage.SaveChangesAsync();
+    }
+
+    public async Task<Person?> GetPerson(Guid id)
+    {
+        var person = await storage.Persons.Where(person => person.Id == id).FirstOrDefaultAsync();
+        return person;
+    }
+    
+    public async Task<IList<Person>> SearchPersonsByName(string name)
+    {
+        var persons = await storage.Persons.Where(person => person.FirstName == name).ToListAsync();
+        return persons;
+    }
+    
+    public async Task Delete(Guid id)
+    {
+        await storage.Persons.Where(person => person.Id == id).ExecuteDeleteAsync();
+        await storage.SaveChangesAsync();
+    }
+}
