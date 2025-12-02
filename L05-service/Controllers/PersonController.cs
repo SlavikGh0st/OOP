f10ec90e87b340b43e7cb99bdaffaa6c3099e733
antvu <antvu@skbkontur.ru>
2025-12-02 15:44:56 +0500

    Добавил занятие по WEB API

---
+++ L05-service/Controllers/PersonController.cs
@@ -0,0 +1,50 @@
+﻿using L05_service.Models;
+using L05_service.Storage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L05_service.Controllers;
+
+//[ApiController]
+//[Route("route")] //нужно придумать путь для доступа к этим методам 
+public class PersonController : ControllerBase
+{
+    private readonly PersonStorage storage;
+
+    public PersonController()
+    {
+        var db = new Db();
+        storage = new PersonStorage(db);
+    }
+
+    //тип HTTP запроса
+    //маршрут Route
+    //описание ответов
+    public async Task<ActionResult<IList<Person>>> SearchPersons( /* что-то должно быть на входе */)
+    {
+        throw new NotImplementedException();
+    }
+
+    //тип HTTP запроса
+    //маршрут Route
+    //описание ответов
+    public async Task<ActionResult<Person>> CreatePerson( /* что-то должно быть на входе */)
+    {
+        throw new NotImplementedException();
+    }
+
+    //тип HTTP запроса
+    //маршрут Route
+    //описание ответов
+    public async Task<ActionResult<Person>> GetPerson( /* что-то должно быть на входе */)
+    {
+        throw new NotImplementedException();
+    }
+
+    //тип HTTP запроса
+    //маршрут Route
+    //описание ответов
+    public async Task<ActionResult> DeletePerson( /* что-то должно быть на входе */)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ L05-service/Storage/PersonStorage.cs
@@ -0,0 +1,36 @@
+﻿using L05_service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L05_service.Storage;
+
+public class PersonStorage
+{
+    private readonly Db storage;
+
+    public PersonStorage(Db storage)
+    {
+        this.storage = storage;
+    }
+
+    public async Task<bool> CreatePerson(Person person)
+    {
+        if (await GetPerson(person.Id) != null)
+            return false;
+
+        await storage.AddAsync(person);
+        await storage.SaveChangesAsync();
+        return true;
+    }
+
+    public Task<Person?> GetPerson(Guid id) =>
+        storage.Persons.FirstOrDefaultAsync(person => person.Id == id);
+
+    public Task<List<Person>> SearchPersons(PersonSearchFields searchFields) =>
+        storage.Persons.Where(person => searchFields.Name == null || person.FirstName == searchFields.Name).ToListAsync();
+
+    public async Task Delete(Guid id)
+    {
+        await storage.Persons.Where(person => person.Id == id).ExecuteDeleteAsync();
+        await storage.SaveChangesAsync();
+    }
+}
